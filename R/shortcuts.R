ea6c9e7e00ea48a46e5fc09c869f700913d67dbc
Jelle Goeman <j.j.goeman@lumc.nl>
2011-03-08 00:00:00 +0000

    version 0.1-3

--- R/shortcuts.R
+++ R/shortcuts.R
@@ -1,6 +1,7 @@
-pickFisher <- function(all, select = seq_along(all), alpha=0.05, silent=FALSE) {
-  rej <- all[select]
-  nr <- setdiff(all, rej)
+pickFisher <- function(p, select = seq_along(p), alpha=0.05, silent=FALSE) {
+
+  rej <- p[select]
+  nr <- setdiff(p, rej)
   nr <- nr[nr > min(rej)]
   lrej <- -2*log(sort(rej, decreasing=TRUE))
   lnr <- -2*log(sort(nr, decreasing=TRUE))
@@ -20,55 +21,138 @@ pickFisher <- function(all, select = seq_along(all), alpha=0.05, silent=FALSE) {
 }
 
 
-curveFisher <- function(all, alpha=0.05) {
-  lpv <- -2*log(sort(all))
-  res <- numeric(length(lpv))
+curveFisher <- function(p, select = seq_along(p), alpha=0.05, plot = TRUE) {
+
+  ranks <- sort(rank(p, ties="first")[select])
+  others <- setdiff(length(p):1, ranks)
+  lpv <- -2*log(sort(p))
+  res <- numeric(length(ranks))
   chisqs <- qchisq(1-alpha, df=2*1:length(lpv), lower=T)
   st <- 1
-  ed <- 1
-  while (ed <= length(lpv)) {
-    ins <- seq(st,ed)
-    outs <- rev(seq_along(lpv)[-(1:ed)])
+  for (ed in 1:length(ranks)) {
+    ins <- ranks[seq(st,ed)]
+    outs <- setdiff(length(lpv):min(ins), ins)
     cr.v <- max(chisqs[ed-st+1+0:length(outs)] - cumsum(c(0,lpv[outs])))
     rej <- (sum(lpv[ins]) >= cr.v)
-    if (rej & (st < ed)) {
+    if (rej)
       st <- st+1
-    } else if (rej) {
-      res[ed] <- st
-      ed <- ed + 1
-    } else {
-      res[ed] <- st - 1
-      ed <- ed + 1
-    }
+    res[ed] <- st-1
   }
   names(res) <- names(lpv)
-  res
+  if (plot) {
+    false <- c(0, res)
+    xs <- 1:length(false)-.5
+    tots <- 0:length(res)
+    plot(xs, tots, type="S", xlab="number of rejections", ylab="number of rejections", lty=2)
+    lines(xs, false, type="S")
+    legend("topleft", c(paste("correct rejections (", 100*(1-alpha), "% conf.)", sep=""),"others"), lty=1:2)
+    invisible(res)
+  } else
+    res
+
 }
 
 
-pickSimes <- function(all, select = seq_along(all), alpha=0.05, hommel = FALSE, silent=FALSE) {
-  rej <- sort(all[select], decreasing=TRUE)
-  nr <- sort(setdiff(all, rej), decreasing=TRUE)
-  nr <- nr[nr > min(rej)]
-  if (!hommel)
-    res <- which(!sapply(1:length(rej), function(s)
-      all(sapply(0:length(nr), function(i)
-        any(sort(c(rej[1:s], nr[seq_len(i)])) <= alpha*(1:(s+i))/(s+i) )
-      ))
-    ))
-  else
-    res <- which(!sapply(1:length(rej), function(s)
-      all(sapply(0:length(nr), function(i)
-        any(sort(c(rej[1:s], nr[seq_len(i)]))*sum(1/(1:(s+i))) < alpha*(1:(s+i))/(s+i) )
-      ))
-    ))
-  out <- max(c(0,res))
+pickSimes <- function(p, select = seq_along(p), alpha=0.05, hommel=FALSE, silent=FALSE) {
+
+  ranks <- sort(rank(p, ties="first")[select])
+  p <- sort(p)
+  others <- setdiff(length(p):1, ranks)
+  st <- 1
+  ed <- length(ranks)
+  ready <- FALSE
+  while (!ready) {
+    ins <- seq_along(p) %in% ranks[seq(st,ed)]
+    outs <- (!logical(length(p))) & (cummax(ins)==1) & (!ins)
+    participate <- numeric(length(p))
+    participate[ins] <- 1+sum(outs)
+    participate[outs] <- seq_len(sum(outs))
+    maxlag <- cumsum(outs)
+    rej <- TRUE
+    i <- 0
+    while (rej && (i <= sum(outs))) {
+      bottom.is <- (participate > i)
+      K <- sum(bottom.is)
+      if (hommel)
+        lag <- floor(1:K - p[bottom.is]/(alpha/(K*sum(1/1:K))))
+      else
+        lag <- floor(1:K - p[bottom.is]/(alpha/K))
+      if (any(lag >= 0 & lag >= maxlag[bottom.is] - i & ins[bottom.is]))
+        i <- Inf
+      else if (any(lag >= 0))
+         i <- i + 1 + max(pmin(lag, maxlag[bottom.is] - i))
+      else
+        rej <- FALSE
+    }
+    if (rej) {
+      st <- st+1
+      ready <- st > ed
+    } else
+      ready <- TRUE
+  }
+  out <- ed-st+1
   if (!silent) {
-    cat("Rejected ", length(rej), " hypotheses at confidence level ", 1-alpha, ".\n", sep="")
-    cat("Correct rejections >= ", length(rej)-out, "; ", sep="")
+    cat("Rejected ", length(ranks), " hypotheses. At confidence level ", 1-alpha, ":\n", sep="")
+    cat("Correct rejections >= ", length(ranks)-out, "; ", sep="")
     cat("False rejections <= ", out, ".\n", sep="")
     invisible(out)
   } else
     out
 }
+
+
+curveSimes <- function(p, select = seq_along(p), alpha=0.05, hommel=FALSE, plot = TRUE) {
+
+  ranks <- sort(rank(p, ties="first")[select])
+  p <- sort(p)
+  others <- setdiff(length(p):1, ranks)
+  res <- numeric(length(ranks))
+  st <- 1
+  for (ed in 1:length(ranks)) {
+    ins <- seq_along(p) %in% ranks[seq(st,ed)]
+    outs <- (!logical(length(p))) & (cummax(ins)==1) & (!ins)
+    participate <- numeric(length(p))
+    participate[ins] <- 1+sum(outs)
+    participate[outs] <- seq_len(sum(outs))
+    maxlag <- cumsum(outs)
+    rej <- TRUE
+    i <- 0
+    while (rej && (i <= sum(outs))) {
+      bottom.is <- (participate > i)
+      K <- sum(bottom.is)
+      if (hommel)
+        lag <- floor(1:K - p[bottom.is]/(alpha/(K*sum(1/1:K))))
+      else
+        lag <- floor(1:K - p[bottom.is]/(alpha/K))
+      if (any(lag >= 0 & lag >= maxlag[bottom.is] - i & ins[bottom.is]))
+        i <- Inf
+      else if (any(lag >= 0))
+         i <- i + 1 + max(pmin(lag, maxlag[bottom.is] - i))
+      else
+        rej <- FALSE
+    }
+    if (rej)
+      st <- st+1
+    res[ed] <- st-1
+  }
+  names(res) <- names(p[ranks])
+  if (plot) {
+    false <- c(0, res)
+    xs <- 1:length(false)-.5
+    tots <- 0:length(res)
+    plot(xs, tots, type="S", xlab="number of rejections", ylab="number of rejections", lty=2)
+    lines(xs, false, type="S")
+    legend("topleft", c(paste("correct rejections (", 100*(1-alpha), "% conf.)", sep=""),"others"), lty=1:2)
+    invisible(res)
+  } else
+    res
+}
+
+
+
+
+
+
+
+
 
